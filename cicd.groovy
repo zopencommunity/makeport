5bddb70901fabcacd6743fb2c16494d6c9c2ab5c
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-19 21:21:34 -0400

    Remove polling of utils.git

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'makeport'), string(name: 'DESCRIPTION', 'GNU Make is a tool which controls the generation of executables and other non-source files of a program from the program's source files.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'makeport'), string(name: 'DESCRIPTION', value: 'GNU Make is a tool which controls the generation of executables and other non-source files of a program from the program's source files.' )]
         }
 }
