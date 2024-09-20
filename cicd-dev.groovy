cf1b5235f4e8f8901160c1ff13d12e799db638d3
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:35:54 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -16,12 +16,12 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/makeport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/makeport.git']]])
   }
 
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [
-    string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/makeport.git'),
+    string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/makeport.git'),
     string(name: 'PORT_DESCRIPTION', value: 'GNU Make is a tool which controls the generation of executables and other non-source files of a program from program source files.'),
     string(name: 'BUILD_LINE', value: 'DEV')]
   }
